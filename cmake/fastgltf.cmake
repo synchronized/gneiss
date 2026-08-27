ec222ce77fa85b493d99e5cd172251dd340b34cf
synchronized <synchronized@yeah.net>
2026-08-27 15:05:33 +0800

    fix(build): keep fastgltf out of install package

--- cmake/fastgltf.cmake
+++ cmake/fastgltf.cmake
@@ -24,18 +24,31 @@ function(gneiss_resolve_fastgltf)
   set(FASTGLTF_COMPILE_AS_CPP20 ON CACHE BOOL "" FORCE)
   set(FASTGLTF_ENABLE_CPP_MODULES OFF CACHE BOOL "" FORCE)
 
+  set(gneiss_fastgltf_exclude_from_all)
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.28)
+    list(APPEND gneiss_fastgltf_exclude_from_all EXCLUDE_FROM_ALL)
+  endif()
   FetchContent_Declare(
     fastgltf
     GIT_REPOSITORY "${GNEISS_FASTGLTF_GIT_REPOSITORY}"
     GIT_TAG "${GNEISS_FASTGLTF_GIT_TAG}"
     GIT_SHALLOW FALSE
     GIT_PROGRESS TRUE
+    ${gneiss_fastgltf_exclude_from_all}
   )
   set(gneiss_saved_build_shared_libs "${BUILD_SHARED_LIBS}")
   set(gneiss_saved_disable_find_simdjson "${CMAKE_DISABLE_FIND_PACKAGE_simdjson}")
   set(BUILD_SHARED_LIBS OFF)
   set(CMAKE_DISABLE_FIND_PACKAGE_simdjson ON)
-  FetchContent_MakeAvailable(fastgltf)
+  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.28)
+    FetchContent_MakeAvailable(fastgltf)
+  else()
+    FetchContent_GetProperties(fastgltf)
+    if(NOT fastgltf_POPULATED)
+      FetchContent_Populate(fastgltf)
+      add_subdirectory("${fastgltf_SOURCE_DIR}" "${fastgltf_BINARY_DIR}" EXCLUDE_FROM_ALL)
+    endif()
+  endif()
   set(BUILD_SHARED_LIBS "${gneiss_saved_build_shared_libs}")
   set(CMAKE_DISABLE_FIND_PACKAGE_simdjson "${gneiss_saved_disable_find_simdjson}")
 
